13be64cd2f595585da4aae549df34eb925df1912
muhaiminulmugdha <muhaiminulmugdha@gmail.com>
2025-01-17 14:50:43 -0500

    finished login page, added new landing page, made signup page did the css but some left

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.listen(3000, ()=>{
 });
 
 app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.get('/login',(req,res)=>{
     res.render('login');
 });
 
+// app.get('/signup',(req, res) => {
+//     res.render('signup');
+// })
+
+app.get('/signup',(req,res)=>{
+    res.render('signup');
+});
