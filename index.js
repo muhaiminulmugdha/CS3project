ed423ef5a522831b091810748dad920f7189f471
muhaiminulmugdha <muhaiminulmugdha@gmail.com>
2025-01-28 14:23:06 -0500

    added - about - section, did some css on it, fixed landing page nav bar

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.set('view engine', 'ejs');
 
 app.use("/api/v1/user", require('./routes/userRoutes'));
 
-mySqlPool
-    .query("SELECT 1")
-    .then(() => {
-        console.log("Successfully connected to the database");
-    }).catch((error) => {
-        console.error(error);
-})
+// mySqlPool
+//     .query("SELECT 1")
+//     .then(() => {
+//         console.log("Successfully connected to the database");
+//     }).catch((error) => {
+//         console.error(error);
+// })
 
 app.listen(3000, ()=>{
     console.log("server is running on port 3000");
@@ -22,6 +22,10 @@ app.listen(3000, ()=>{
 app.get('/',(req,res)=>{
     res.render('index');
 });
+
+app.get("/about", (req, res) => {
+    res.render('about');
+})
 
 app.get('/login',(req,res)=>{
     res.render('login');
