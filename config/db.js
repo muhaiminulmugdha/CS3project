41188b1b229835e482e41009f3c77e9805646a48
muhaiminulmugdha <muhaiminulmugdha@gmail.com>
2025-03-23 22:05:29 -0400

    connected database, able to put data into the database, but can't retrieve yet

--- config/db.js
+++ config/db.js
@@ -1,10 +1,30 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
-const mysqlPool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "bc_db",
-}).promise()
+// Create the connection pool
+const pool = mysql.createPool({
+    host: 'localhost',        // Database host (localhost if running locally)
+    user: 'root',             // MySQL username (replace with your MySQL username)
+    password: 'yourpassword', // MySQL password (replace with your MySQL password)
+    database: 'falconflowdb', // The name of your database
+    waitForConnections: true,
+    connectionLimit: 10,      // The max number of simultaneous connections
+    queueLimit: 0
+});
 
-// const result = await pool.query("SELECT * FROM users");
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Database connection failed:');
+    } else {
+        console.log('Connected to MySQL database!');
+        connection.release(); // Always release the connection back to the pool
+    }
+});
+
+
+
+
+// Export the pool to be used in other files (promise-based)
+module.exports = pool.promise();
+
+
--- index.js
+++ index.js
@@ -1,41 +1,60 @@
-const express= require('express');
+const express = require('express');
 const app = express();
-const mySqlPool = require("./config/db.js")
+const bodyParser = require('body-parser');
+// const userRoutes = require('./routes/users/userRoutes');
+const questionRoutes = require('./routes/questionsRoutes');
 
-app.use(express.static('public'));
+// const commentRoutes = require('./routes/commentsRoutes');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
 app.set('view engine', 'ejs');
+// app.use("views","./views");
 
-app.use("/api/v1/user", require('./routes/userRoutes'));
+// Serve static files (CSS, images, etc.)
+app.use(express.static('public'));
 
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000");
-});
+// Use routes
+app.use("/", questionRoutes);
 
-app.get('/',(req,res)=>{
+
+
+
+app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get("/about", (req, res) => {
     res.render('about');
-})
+});
 
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
     res.render('login');
 });
 
-
-app.get('/signup',(req,res)=>{
+app.get('/signup', (req, res) => {
     res.render('signup');
 });
 
-app.get('/ask_bros', (req, res) => {
-    res.render('ask_bros');
+// app.get('/ask_bros', (req, res) => {
+//     res.render('ask_bros');  // This renders the form for asking questions
+// });
+
+// app.get('/help_bros', (req, res) => {
+//     res.render('help_bros');  // This renders the feed of questions
+// });
+
+
+// Start the server
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });
 
-app.get('/help_bros', (req, res) => {
-    res.render('help_bros');
-});
 
-app.get('/comment_here', (req, res) => {
-    res.render('comment_here');
-});
+
+
