de2c3684965c094fd1b900c176f89ac697269c36
Muhtasim <162151530+Muhtasim19@users.noreply.github.com>
2026-04-18 21:00:11 -0400

    feat: add additional developer email for testing purposes

--- config/devEmail.js
+++ config/devEmail.js
@@ -2,7 +2,8 @@ const devEmails = [
     'teacher1@cpsd.us',
     '27mhaq@cpsd.us',
     'admin@cpsd.us',
-    'wimosi7701@bmoar.com'
+    'wimosi7701@bmoar.com',
+    'ahnafakif009@gmail.com'
     // Add any teacher/admin emails here
 ];
 
